0f5fb0ad307957401f5319053e147811d9bb6bdc
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-07 13:08:01 -0400

    Added files and folders for L5 memory game. Updated index/html/css for gameboard. Implemented shuffle algorithm.

---
+++ Lab5/MemoryGame/scripts/script.js
@@ -0,0 +1,91 @@
+
+let gameTiles = []
+let gameBoard = []
+let matches = [];
+const images = [    
+                    "resources/images/cap.jpg",
+                    "resources/images/check.png",
+                    "resources/images/galaxy.jpg",
+                    "resources/images/letters.jpg",
+                    "resources/images/lion.png",
+                    "resources/images/present.png",
+                    "resources/images/rocket.png",
+                    "resources/images/sqirrel.png",
+                ]
+
+window.onload = function(){
+    document.querySelectorAll('#board td')
+    .forEach(e => e.addEventListener("click", function() {
+
+        let id = this.id;
+        //check 
+        console.log("Clicked " + id);
+        
+        //if element has not been clicked
+        if(!matches.includes(id)){
+            //Run
+            runCheck(id);
+        } else{
+            console.log("already taken");
+            console.log("Matches contains: " + matches);
+            return false;
+        }
+    }));
+
+    //button reset logic
+    document.querySelectorAll('#button')
+    .forEach(e => e.addEventListener("click", function() {
+        resetBoard();
+    }));
+
+    setGameBoard(images);
+    shuffleTiles();
+}
+
+function resetBoard(){
+    window.location.reload(true);
+}
+
+function setGameBoard(images){
+    for (let i = 0; i <= images.length - 1; i++){
+        for(let j = 0; j < 2; j++){
+            gameTiles.push(images[i])
+        }
+    }
+    console.log("Before shuffle");
+    runCheck(gameTiles);
+}
+
+function shuffleTiles(){
+    //appending to board
+    console.log("Shuffling...");
+    const board = document.querySelector("td");
+    let shuffledCards = shuffle(gameTiles);
+    runCheck(shuffledCards);
+    for (let i = 0; i < shuffledCards.length; i++){
+    //each element in array
+         //console.log(item);
+    };
+}
+
+//DURSTENFELD FOR ES6
+//source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+//-->   https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); //round down, whole number
+        [array[i], array[j]] = [array[j], array[i]]; //swap values
+        
+    }
+    //swapped values
+    console.log("After shuffle");
+    console.log(array);
+    return array;
+}
+
+function runCheck(array){
+    console.log(array.length);
+    for(let i = 0; i < array.length - 1; i++){
+        console.log(array[i])
+    }
+}
