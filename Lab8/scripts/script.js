976cc8e720b895590f325ff6ad465c1b1b2aa807
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-20 10:45:53 -0400

    Updated buttons to have ids. Added click events for each button.

--- Lab8/scripts/script.js
+++ Lab8/scripts/script.js
@@ -1,7 +1,51 @@
 $(function() {
 
-    //set div styles
+    //attach event to each button
+
+    //set div styles for containers and box
     $("#canvas").height(800).width(1000).css({"margin" : "0 auto",});
     $("#main-div").height(500).width(700).css({"background-color" : "#d18cbc","margin" : "0 auto",})
     $("#button-content").css({"text-align" : "center", "margin-top" : "20px", "margin-bottom" : "20px",});
+    $("#main-box").height(100).width(100).css({"background-color" : "#0ffeff"});
+
+    //animations
+    //up
+    $("#button-1").on("click", function(){
+
+    });
+
+    //down
+    $("#button-2").on("click", function(){
+
+    });
+
+    //left
+    $("#button-3").on("click", function(){
+
+    });
+
+    //right
+    $("#button-4").on("click", function(){
+
+    });
+
+    //fadeout
+    $("#button-5").on("click", function(){
+
+    });
+
+    //fadein
+    $("#button-6").on("click", function(){
+
+    });
+
+    //blink
+    $("#button-7").on("click", function(){
+
+    });
+
+    //reset
+    $("#button-8").on("click", function(){
+
+    });
 });
