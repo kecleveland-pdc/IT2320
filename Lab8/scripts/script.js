8ae873ff7a3f506b8e77961e270318dd7d19cb56
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-20 10:22:27 -0400

    Updated to use jQuery .css instead of css styles.

---
+++ Lab8/scripts/script.js
@@ -0,0 +1,7 @@
+$(function() {
+
+    //set div styles
+    $("#canvas").height(800).width(1000).css({"margin" : "0 auto",});
+    $("#main-div").height(500).width(700).css({"background-color" : "#d18cbc","margin" : "0 auto",})
+    $("#button-content").css({"text-align" : "center", "margin-top" : "20px", "margin-bottom" : "20px",});
+});
