87820a58b3a8a6ecdb0de683e332d2e8a941fab1
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-22 18:41:56 -0400

    Removed Bootstrap. Adding in checks for box moving beyond parent div.

--- Lab8/scripts/script.js
+++ Lab8/scripts/script.js
@@ -11,65 +11,72 @@ $(function() {
 
     let position = $("#main-box").position(); //main-box position
     let originalPosition = $("#main-box").offset(); 
-    let stopBlink = null;
+    let animation = null;
     console.log("Left: " + position.left + " Top: " + position.top)
+    console.log("Orig: " + originalPosition.left + "--" + originalPosition.top);
 
     //animations
     //up
     $("#button-1").on("click", function(){
-        let position = $("#main-box").position(); //main-box position
-        $("#main-box").animate({top : "-=20%"});
-        console.log(position.top);
+        clearInterval(animation);
+        animation = setInterval(function(){
+            $("#main-box").animate({top : "-=10%"});
+        }, 500);
     });
 
     //down
     $("#button-2").on("click", function(){
-        let position = $("#main-box").position(); //main-box position
-        $("#main-box").animate({top : "+=20%"});
-        console.log(position.top);
+        clearInterval(animation);
+        animation = setInterval(function(){
+            $("#main-box").animate({top : "+=10%"});
+        }, 500);
     });
 
     //left
     $("#button-3").on("click", function(){
-        let position = $("#main-box").position(); //main-box position
-        $("#main-box").animate({left : "-=20%"});
-        console.log(position.left) 
+        clearInterval(animation);
+        let curPosition = $("#main-box").position();
+        animation = setInterval(function(){
+            //clear interval if past container
+            $("#main-box").animate({left : "-=10%"});
+            curPosition = $("#main-box").position();
+            console.log("Cur " + Math.floor(curPosition.left) + " Original: " + Math.floor(originalPosition.left));
+        }, 500);
     });
 
     //right
     $("#button-4").on("click", function(){
-        let position = $("#main-box").position(); //main-box position
-        $("#main-box").animate({left : '+=20%'});
-        console.log(position.left)
+        clearInterval(animation);
+        animation = setInterval(function(){
+            $("#main-box").animate({left : '+=10%'});
+        }, 500);
     });
 
     //fadeout
     $("#button-5").on("click", function(){
+        clearInterval(animation);
         $("#main-box").fadeOut();
     });
 
     //fadein
     $("#button-6").on("click", function(){
-        {
-            event.preventDefault();
-        }
-
+        clearInterval(animation);
         $("#main-box").fadeIn();
     });
 
     //blink
     $("#button-7").on("click", function(){
-        stopBlink = setInterval(function(){
+        clearInterval(animation);
+        animation = setInterval(function(){
             $("#main-box").fadeOut("fast").fadeIn("fast");
         }, 500);
     });
 
     //reset
     $("#button-8").on("click", function(){
+        clearInterval(animation);
         const offset = $("#main-div").offset();
-        clearInterval(stopBlink);
-        let position = $("#main-box").position(); //main-box position
         $("#main-box").css({top: originalPosition.top, left: originalPosition.left });
-        console.log(position.top);
+        $("#main-box").show();
     });
 });
