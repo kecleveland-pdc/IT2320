277edefa9cedafc732673d129ef55c6a3ee4b836
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-12-06 21:40:37 -0500

    Updating logic for gameplay.

--- Lab3/scripts/script.js
+++ Lab3/scripts/script.js
@@ -1,8 +1,8 @@
 
 let gameBoard = [];
-let currentPlayer = null;
 const player1 = {name: "Player1", piece: "X"};
 const computer = {name: "Computer", piece: "O"};
+let currentPlayer = player1
 var winningState = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,7 +14,6 @@ var winningState = [
     [2, 4, 6]
 ]
 
-
 window.onload = function(){
     document.querySelectorAll('#board td')
     .forEach(e => e.addEventListener("click", function() {
@@ -22,7 +21,6 @@ window.onload = function(){
         let id = this.id;
         //check 
         console.log("Clicked " + id);
-        
         //if element has not been clicked
         if(!gameBoard.includes(id)){
             //Run
@@ -42,25 +40,47 @@ window.onload = function(){
 }
 
 function runGame(id){
-    if(currentPlayer == null || currentPlayer.name == "Computer"){
-        currentPlayer = player1;
-    }else{
-        currentPlayer = computer
-    }
-    console.log(currentPlayer);
+    console.log("CurrentPlayer " + currentPlayer.name);
     selectBox(id, currentPlayer);
-    checkGameState();
+    console.log("Current Player: " + currentPlayer.name);
+    if (currentPlayer.Name == 'Computer'){
+        Console.log("Computer playing");
+        computerTurn()
+        currentPlayer = player1;
+    }
 }
 
 function selectBox(id, currentPlayer) {
-    gameBoard.push(id);
-    let element = document.getElementById(id);
-    element.classList.add("td-background");
-    element.innerHTML = currentPlayer.piece;
+    if (currentPlayer == player1){
+        gameBoard.push(id);
+        let element = document.getElementById(id);
+        element.classList.add("td-background");
+        element.innerHTML = currentPlayer.piece;
+        checkGameState();
+    }
 
-    //check 
+    console.log("Handing control to computer");
+    currentPlayer = computer;
     console.log("Current id is: " + id)
     console.log("Gameboard contains: " + gameBoard);
+}
+
+function computerTurn(){
+    for(let i=0; i < 9; i++){
+        if(!(gameBoard[i] != 'undefined')){
+            gameBoard.push(i);
+            let element = document.getElementById(i);
+            element.classList.add("td-background");
+            element.innerHTML = currentPlayer.piece;
+        
+            //check 
+           // console.log("Current id is: " + i)
+           // console.log("Gameboard contains: " + gameBoard);
+            checkGameState();
+        }else{
+            console.log("Gamboard already contains piece " + i)
+        }
+    }
 }
 
 function ResetBoard(){
@@ -68,6 +88,14 @@ function ResetBoard(){
 }
 
 function checkGameState(){
+    if(currentPlayer.name == "Computer"){
+        console.log("Computer turn");
+        computerTurn();
+        currentPlayer = player1
+        return true
+    }else{
+        
+    }
     for(let state in winningState)
     {
         for(let i=0; i < state.length; i++)
